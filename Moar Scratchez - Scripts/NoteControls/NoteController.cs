258e0807aa113f83bc8ccd44c634d57744a9474d
Ting Wen <40506952+tingw55555@users.noreply.github.com>
2020-09-02 16:46:30 -0700

    Adding Moar Scratchez Scripts

---
+++ Moar Scratchez - Scripts/NoteControls/NoteController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NoteController : MonoBehaviour
+{
+    public Image icon;
+    public int barLength;
+
+    public NoteType currentType;
+
+    public float speed;
+
+    public bool hitZone;
+    public bool canBePressed;
+
+
+    //public bool isHold;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hitZone = false;
+        canBePressed = false;
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+
+        Move();
+
+
+
+
+    }
+
+    public void Move()
+    {
+        transform.localPosition += (Vector3.right * speed * Time.deltaTime);
+    }
+
+
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    //When the note enters the zones, it can be pressed. 
+    //    //But it will result in a miss if the note is not in the hit zone
+
+    //    //MissZone miss = collision.GetComponent<MissZone>();
+    //    HitZone hit = collision.GetComponent<HitZone>();
+
+    //    BeatManager.instance.canBePressed = true;
+
+    //    if (hit != null)
+    //    {
+    //        hitZone = true;
+    //    }
+
+    //}
+
+
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    BeatManager.instance.canBePressed = false;
+    //    hitZone = false;
+    //}
+
+
+
+    public virtual void InitializeBar() { }
+}
+
+
+
---
+++ Moar Scratchez - Scripts/NoteControls/Zones.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Zones : MonoBehaviour
+{
+    public Image image;
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    image.enabled = true;   
+    //}
+
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    image.enabled = false;
+    //}
+}
