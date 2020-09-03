833a667a839942accea21121b4cd2f18b5ebdcb9
twencea <twen05@thecea.ca>
2020-09-02 17:46:06 -0700

    Added Spherezilla

---
+++ Spherezilla/Units/Battleship.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battleship : MonoBehaviour
+{
+    //public Vector3 spawnPosition;
+
+    //private void Start()
+    //{
+
+    //    spawnPosition = transform.position;
+
+    //    //MoveToTargetPosition();
+    //}
+
+    //private void Update()
+    //{
+    //    transform.position += -transform.right * moveSpeed * Time.deltaTime;
+    //}
+
+
+    //public void MoveToTargetPosition()
+    //{   
+    //    StartCoroutine(MoveCoroutine());
+    //}
+
+    
+
+}
